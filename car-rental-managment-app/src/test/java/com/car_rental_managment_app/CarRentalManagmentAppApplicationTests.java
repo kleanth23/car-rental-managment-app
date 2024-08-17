25d86c501629ed2459e1f03e25320eb0c415057d
kleanth23 <kleanthpollo20@gmail.com>
2024-08-17 11:12:35 +0200

    first commit

---
+++ car-rental-managment-app/src/test/java/com/car_rental_managment_app/CarRentalManagmentAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.car_rental_managment_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRentalManagmentAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
